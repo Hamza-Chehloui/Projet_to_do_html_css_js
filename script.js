4866317ef9b7265f36f248bb54dbf6c3739f0ee2
Hamza <hamzacoude@gmail.com>
2024-10-31 21:36:16 +0100

    modified script javascript and ajouter button anuler

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function showElement(){
+let mybtn =document.getElementById("btn");
+
+
+mybtn.onclick = function(){
     document.getElementById('pageadd_tach').classList.remove('hidden');
-    
 }
+let myanul =document.getElementById("anuler");
+
+myanul.onclick =function(){
+    document.getElementById('pageadd_tach').classList.add('hidden');
+}
+
