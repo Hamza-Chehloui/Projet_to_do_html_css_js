d94b92d62c5c9ef8b0954cc8ea4bd08164d9c308
Hamza <hamzacoude@gmail.com>
2024-10-31 22:55:11 +0100

    modified script javascript

--- script.js
+++ script.js
@@ -9,4 +9,14 @@ let myanul =document.getElementById("anuler");
 myanul.onclick =function(){
     document.getElementById('pageadd_tach').classList.add('hidden');
 }
+let mysub =document.getElementById("submit");
+mysub.onclick= function(){
+    const inputtext =document.getElementById("validationCustom03").value;
+    document.getElementById("div1").innerText=inputtext;
+    const inputdescrep =document.getElementById("validationTextarea").value;
+    document.getElementById("div2").innerText=inputdescrep;
+    const inputdat =document.getElementById("dat").value;
+    document.getElementById("div3").innerText=inputdat;
+    document.getElementById('pageadd_tach').classList.add('hidden');
+}
 
