11d5633a00679713f70df558f65b9066689f69c4
Hamza <hamzacoude@gmail.com>
2024-11-01 23:39:33 +0100

    modiefied script javascript

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 let mybtn =document.getElementById("btn");
-
-
 mybtn.onclick = function(){
     document.getElementById('pageadd_tach').classList.remove('hidden');
 }
@@ -9,16 +7,17 @@ let myanul =document.getElementById("anuler");
 myanul.onclick =function(){
     document.getElementById('pageadd_tach').classList.add('hidden');
 }
-let mysub =document.getElementById("submit");
-mysub.onclick= function(){
-    let inputtext =document.getElementById("validationCustom03").value;
-    document.getElementById("titre").innerHTML=inputtext;
-    // let inputdescrep =document.getElementById("validationTextarea").value;
-    // document.getElementById("Description").innerHTML=inputdescrep;
-    let inputdat =document.getElementById("dat").value;
-    document.getElementById("datetach").innerHTML=inputdat;
-    document.getElementById('pageadd_tach').classList.add('hidden');
+
+let mysub =document.getElementById("subm");
+mysub.onclick =function(){
+    document.getElementById('cont_todo').classList.remove('container_todo');
+    let input =document.getElementById("validationCustom03").value;
+    let container =document.getElementById("titer_todo").innerText+=input;
+    
+    document.getElementById('pageadd_tach').classList.add('hidden'); 
 }
+
+
 
 
 
